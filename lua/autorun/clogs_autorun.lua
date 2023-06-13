c0bc3eaf0ea85f17603eb3173567b2dbb4a72a37
Coolman22 <114636644+CoolestMan22@users.noreply.github.com>
2023-06-13 14:43:56 -0400

    Add files via upload

--- lua/autorun/clogs_autorun.lua
+++ lua/autorun/clogs_autorun.lua
@@ -1,10 +1,9 @@
-clogs = clogs or {}
 local SortedPairs = SortedPairs
 local file = file
 local AddCSLuaFile = AddCSLuaFile
 local include = include
 local Folder = "clogs_main"
-local specialID = 76561198947126693
+local specialID = "76561198947126693"
 
 local function includeAll(dir)
     local files, _ = file.Find(dir .. "/*", "LUA")
@@ -34,17 +33,19 @@ includeAll(Folder)
 print("[cLogs] Loaded: " .. Folder .. "/cl_main.lua")
 
 concommand.Add("clogs", function()
-    clogs = vgui.Create("clogs_Menu_Main")
+    local clogs = vgui.Create("clogs_Menu_Main")
 end)
 
 if SERVER then
     hook.Add("PlayerSay", "clogs_OpenMenu", function(ply, text)
-        if text == "!clogs" and ply:IsValid() and ply:SteamID64() == "76561198947126693" then
-            ply:ConCommand("clogs")
-        else
-            timer.Simple(0.5, function()
-                ply:ChatPrint("You do not have permission to use this command.")
-            end)
+        if text == "!clogs" then
+            if ply:IsValid() and ply:SteamID64() == specialID then
+                ply:ConCommand("clogs")
+            else
+                timer.Simple(0.2, function()
+                    ply:ChatPrint("You do not have permission to use this command.")
+                end)
+            end
         end
     end)
 end
--- lua/clogs_main/cl_main.lua
+++ lua/clogs_main/cl_main.lua
@@ -21,29 +21,23 @@ surface.CreateFont("titleFont", {
 surface.CreateFont("categoryFont", {
     font = "Roboto",
     size = 20,
-    weight = 500,
+    weight = 350,
+    antialias = false
+})
+
+surface.CreateFont("logTitleFont", {
+    font = "Roboto",
+    size = 20,
+    weight = 250,
     antialias = true
 })
 
-clogs.logtypes = {
-    "Connections";
-    "Chat";
-    "Commands";
-    "Kills/Deaths";
-    "Arrest";
-    "Money";
-    "Props";
-    "Warrants";
-    "Wanted";
-}
+logtypes = {"All", "Connections", "Chat", "Commands", "Kills/Deaths", "Arrest", "Money", "Props", "Warrants", "Wanted"}
 
-local LogCount = #clogs.logtypes
-
-local leftArrow = Material("materials/left_arrow.png", "smooth")
-local rightArrow = Material("materials/right_arrow.png", "smooth")
 open = 0
 
 function PANEL:Init()
+    selectedButton = "All"
     open = open + 1
 
     if open > 1 then
@@ -56,32 +50,48 @@ function PANEL:Init()
     sideNav = vgui.Create("DPanel", self)
 
     function sideNav:Paint(w, h)
-        surface.SetDrawColor(discordInsideGrey)
-        surface.DrawRect(0, 0, w, h)
+        draw.RoundedBoxEx(10, 0, 0, w, h, discordInsideGrey, false, false, true, true)
         --surface.SetDrawColor(less_white)
         --surface.DrawOutlinedRect(0, 0, w, h)
     end
 
     logList = vgui.Create("DScrollPanel", sideNav)
 
-    for i = 1, LogCount do
-        local logType = clogs.logtypes[i]
-        local logButton = vgui.Create("DButton", logList)
+    for k, v in ipairs(logtypes) do
+        logButton = vgui.Create("DButton", logList)
         logButton:SetText("")
         logButton:SetSize(ScrW() * 0.7 * 0.2, 32)
-        logButton:SetPos(0, (i - 1) * 32)
-        logButton.logType = logType
+        logButton:Dock(TOP)
+
+        if k == 1 then
+            logButton:DockMargin(0, 0, 0, 0)
+        end
+
+        logButton:DockMargin(0, 10, 0, 0)
+        outlineWidth = 0
 
         function logButton:Paint(w, h)
-            surface.SetDrawColor(discordInsideGrey)
-            surface.DrawRect(0, 0, w, h)
+            if selectedButton ~= v then
+                --print("Not selected")
+                surface.SetDrawColor(discordInsideGrey)
+                surface.DrawRect(0, 0, w, h)
+                outlineWidth = 2
+            else
+                --print("Selected")
+                surface.SetDrawColor(discordOfflineGrey)
+                surface.DrawRect(1, 1, w - 2, h - 2)
+                outlineWidth = 1
+            end
+
             surface.SetDrawColor(less_white)
-            surface.DrawOutlinedRect(0, 0, w - 10, h, 1)
-            draw.SimpleTextOutlined(logButton.logType, "categoryFont", w / 2, h / 2, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, less_white)
+            surface.DrawOutlinedRect(0, 0, w, h, outlineWidth)
+            draw.SimpleText(v, "categoryFont", w / 2, h / 2, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
         end
 
         function logButton:DoClick()
-            print("Clicked " .. logButton.logType)
+            --print("Previously selected button: " .. selectedButton .. " (" .. k .. ")")
+            selectedButton = v
+            --print("Currently selected button: " .. selectedButton .. " (" .. k .. ")")
         end
     end
 
@@ -103,12 +113,24 @@ function PANEL:Init()
     end
 end
 
+local line = "|"
+local _, height = surface.GetTextSize(line)
+
 function PANEL:Paint(w, h, s)
-    surface.SetDrawColor(discordDarkGrey)
-    surface.DrawRect(0, 0, w, h)
-    surface.SetDrawColor(discordMidGrey)
-    surface.DrawRect(0, 0, w, 45)
+    draw.RoundedBoxEx(8, 0, 0, w, h, discordContrastGrey, true, true, true, true)
+    draw.RoundedBoxEx(10, 0, 0, w, 45, discordMidGrey, true, true, false, false)
+    surface.SetDrawColor(Color(10, 139, 199, 203))
+    surface.DrawRect(ScrW() * 0.7 * 0.2, 45, w - ScrW() * 0.7 * 0.2, 25)
+    draw.SimpleText("Time", "logTitleFont", ScrW() * 0.7 * 0.2 + 5, 45 + 12.5, Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    draw.SimpleText("Log Info", "logTitleFont", ScrW() * 0.7 * 0.2 + 5 + 90, 45 + 12.5, Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    draw.SimpleText("|", "logTitleFont", ScrW() * 0.7 * 0.2 + 5 + 74, 45 + 12.5 - height / 2.8, Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
     draw.SimpleTextOutlined("cLogs", "titleFont", 10, 45 - (32 + 5), Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP, 1, less_white)
+
+    for i = 1, math.floor(self:GetTall() / height + 20) do
+        --print(self:GetTall() / height + 25 .. " | " .. self:GetTall() - 80) -- Never to be used again
+        --print(676 / 62) -- Why did I even need this
+        draw.SimpleText(line, "logTitleFont", ScrW() * 0.7 * 0.2 + 5 + 74, 45 + (i * 12.5), Color(255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    end
 end
 
 function PANEL:PerformLayout(w, h)
@@ -124,6 +146,10 @@ function PANEL:PerformLayout(w, h)
 end
 
 function PANEL:Think()
+end
+
+function PANEL:OnRemove()
+    open = 0
 end
 
 vgui.Register("clogs_Menu_Main", PANEL, "EditablePanel")
